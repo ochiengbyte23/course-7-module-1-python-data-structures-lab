105f94b4b81c074c70717551230126759075db3a
Bildad Ochieng Masaga <bijasoto@gmail.com>
2026-02-19 23:17:49 +0300

    created function return student record lazily

--- lib/data_generator.py
+++ lib/data_generator.py
@@ -5,4 +5,5 @@ def student_generator(student_list, major):
     Generate student records filtered by major lazily for memory efficiency
     using a Python generator.
     """
-    pass
+    student_record = ( student for student in student_list if student[2].lower() == major.lower())
+    return student_record
