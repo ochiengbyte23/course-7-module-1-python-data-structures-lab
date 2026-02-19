20c7809bc96c55fef6b7edaecaf5cb8b4993c8cb
Bildad Ochieng Masaga <bijasoto@gmail.com>
2026-02-19 23:07:53 +0300

    implemeted set comprehension to return unique majors

--- lib/set_operations.py
+++ lib/set_operations.py
@@ -5,4 +5,5 @@ def unique_majors(student_list):
     Return a set of unique student majors using set comprehension.
     Extract the major field from each student record.
     """
-    pass
+    major_field = {student[2] for student in student_list }
+    return major_field
